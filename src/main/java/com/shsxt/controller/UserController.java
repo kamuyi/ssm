e0a8a47f9d3cfcc18852451d513f3200f71c5c16
kamuyi <2258858667@qq.com>
2018-10-12 11:42:47 +0800

    ssm环境构建

--- src/main/java/com/shsxt/controller/UserController.java
+++ src/main/java/com/shsxt/controller/UserController.java
@@ -15,8 +15,11 @@ package com.shsxt.controller;
 import com.shsxt.po.User;
 import com.shsxt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
